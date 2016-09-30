7c861389cae87ea62eb906e31ad2b05d56eb2ac2
rickerg0 <rickerg0@gmail.com>
2016-09-30 12:57:21 -0400

    Update day2.py

--- 9_30_2016/day2.py
+++ 9_30_2016/day2.py
@@ -1,1 +1,11 @@
 
+create a function that takes a string of numbers as the argument  Ex "123456"
+   1. retrieve each number from the list
+   2. add up all of the numbers
+   3. return the value, sum
+
+ 
+  example
+     "12345" would return 15
+
+
